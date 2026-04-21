a14a6d6367229b08181dfb7c0a428510bf5ff5ac
hwandroid921 <olgksgml@gmail.com>
2026-04-21 12:50:58 +0900

    [2026.04.21 hwan] test.txt fix & spring setting commit

---
+++ connect_park_spring/src/test/java/com/sfpj/connect_park/ConnectParkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sfpj.connect_park;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectParkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
